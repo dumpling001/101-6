06d17b4dbbd78a09c89fbb59457aa72ab705ef92
dumpling001 <dumpling001@gmail.com>
2017-04-01 15:17:49 +0800

    implement is_member_of?(movie)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,8 @@ class User < ApplicationRecord
   has_many :movies
   has_many :posts
   has_many :movie_relationships
-  has_many :participated_movies, :through => :movie_relationships, :source => :movie,
+  has_many :participated_movies, :through => :movie_relationships, :source => :movie
+ def is_member_of?(movie)
+   participated_movies.include?(movie)
+ end
 end
